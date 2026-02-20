b04280e0feeca5a43f24249a8e3969abe6a16e60
alex <907785012@qq.com>
2026-02-20 12:53:25 +0800

    complete section1-10

--- 2026/part1/section10-swc/final/src/main.ts
+++ 2026/part1/section10-swc/final/src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import metadata from './metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
     .addTag('todos')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  // await SwaggerModule.loadPluginMetadata(metadata);
   SwaggerModule.setup('api', app, documentFactory, {
     jsonDocumentUrl: 'swagger/json',
   });
