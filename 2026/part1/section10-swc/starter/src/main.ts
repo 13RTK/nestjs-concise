58185220c47a13d4862cba09c1d3d5e497d674e8
alex <907785012@qq.com>
2026-02-20 12:22:12 +0800

    add starter of section1-10

---
+++ 2026/part1/section10-swc/starter/src/main.ts
@@ -0,0 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todos example')
+    .setDescription('The todo API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000', 'localhost')
+    .addTag('todos')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
