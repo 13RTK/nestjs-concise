c043db43cedee9a0f35e718b9d5ddd0cacc58ed1
alex <907785012@qq.com>
2026-02-26 16:06:57 +0800

    complete section2-6

--- 2026/part2/section6-exception/final/src/app.module.ts
+++ 2026/part2/section6-exception/final/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TodosModule, MikroOrmModule.forRoot()],
+  imports: [TodosModule, MikroOrmModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- 2026/part2/section6-exception/final/src/main.ts
+++ 2026/part2/section6-exception/final/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DbExceptionFilter } from './fliters/db-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.useGlobalFilters(new DbExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
