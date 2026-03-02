d9789fe17b118ccd0b79fc29241819e7300dfebf
alex <907785012@qq.com>
2026-03-02 18:38:20 +0800

    add starter of section2-9

---
+++ 2026/part2/section9-fastify-swagger/starter/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
+import { pinoHttpConfig } from './configs/pino';
+
+@Module({
+  imports: [
+    TodosModule,
+    MikroOrmModule.forRoot(),
+    UsersModule,
+    // LoggerModule.forRoot({
+    //   pinoHttp: pinoHttpConfig,
+    // }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
