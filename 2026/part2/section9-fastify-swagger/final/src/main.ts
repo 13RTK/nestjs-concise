bce50e6d5c5c2d0f7ff7d9efd7b1e50a1523c28a
alex <907785012@qq.com>
2026-03-02 21:33:08 +0800

    complete section2-9

--- 2026/part2/section9-fastify-swagger/final/src/main.ts
+++ 2026/part2/section9-fastify-swagger/final/src/main.ts
@@ -7,6 +7,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   // Express version
@@ -19,6 +20,14 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo app example')
+    .setDescription('The todos API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({
