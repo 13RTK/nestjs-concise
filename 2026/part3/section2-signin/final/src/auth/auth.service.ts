fa7c6aa715420af98c3f29914a8f962afb2c7ef8
alex <907785012@qq.com>
2026-03-04 17:58:37 +0800

    complete section3-2

--- 2026/part3/section2-signin/final/src/auth/auth.service.ts
+++ 2026/part3/section2-signin/final/src/auth/auth.service.ts
@@ -1,10 +1,32 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signIn(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const comparedResult = await bcrypt.compare(pass, user.password);
+    if (!comparedResult) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const { password, ...result } = user;
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return result;
+  }
 
   async signUp(email: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(email);
