a1a8d29484fa9d42b7c5d012e2f8d19dddae071c
alex <907785012@qq.com>
2026-03-07 15:33:23 +0800

    add starter of section3-5

---
+++ 2026/part3/section5-decorator/starter/src/users/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id: number;
+
+  @Property({ unique: true })
+  email: string;
+
+  @Property()
+  password: string;
+}
