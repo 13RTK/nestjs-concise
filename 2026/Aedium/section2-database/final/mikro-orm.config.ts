5bb9a66f9fb07d10d025605333778ca471723b3f
alex <907785012@qq.com>
2026-03-13 17:40:07 +0800

    complete Aedium-2

--- 2026/Aedium/section2-database/final/mikro-orm.config.ts
+++ 2026/Aedium/section2-database/final/mikro-orm.config.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { defineConfig } from '@mikro-orm/postgresql';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Migrator } from '@mikro-orm/migrations';
+import { SeedManager } from '@mikro-orm/seeder';
 
 export default defineConfig({
   // for simplicity, we use the SQLite database, as it's available pretty much everywhere
@@ -10,6 +12,21 @@ export default defineConfig({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
 
+  driverOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+
+  seeder: {
+    path: './seeders', // path to the folder with seeders
+    pathTs: undefined, // path to the folder with TS seeders (if used, you should put path to compiled files in `path`)
+    defaultSeeder: 'DatabaseSeeder', // default seeder class name
+    glob: '!(*.d).{js,ts}', // how to match seeder files (all .js and .ts files, but not .d.ts)
+    emit: 'ts', // seeder generation mode
+    fileName: (className: string) => className, // seeder file naming convention
+  },
+
   metadataProvider: TsMorphMetadataProvider,
 
   // explicitly list your entities - we'll create the User entity next
@@ -18,4 +35,6 @@ export default defineConfig({
   entitiesTs: ['src/**/*.entity.ts'],
   // enable debug mode to log SQL queries and discovery information
   debug: true,
+
+  extensions: [SeedManager, Migrator],
 });
