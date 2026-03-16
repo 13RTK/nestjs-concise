94bf1bc7bbe4f26307ceb02919a5418becf169e0
alex <907785012@qq.com>
2026-03-16 20:38:57 +0800

    add starter of Aedium-5

---
+++ 2026/Aedium/section5-articles/starter/src/users/users.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { User } from './entities/user.entity';
+import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: EntityRepository<User>,
+    private readonly em: EntityManager,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  // Admin
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne(id, {
+      populate: ['articles'],
+      exclude: [
+        'password',
+        'articles.content',
+        'articles.createdAt',
+        'articles.updatedAt',
+      ],
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
