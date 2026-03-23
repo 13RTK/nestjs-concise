ca72c5edc06cb139dee74252df4f1ee6fce1db97
alex <907785012@qq.com>
2026-03-23 13:11:37 +0800

    add starter of Aedium-13

---
+++ 2026/Aedium/section13-admin/starter/src/articles/dto/create-article.dto.ts
@@ -0,0 +1,24 @@
+import { ArticleStatus } from '../entities/article.entity';
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateArticleDto {
+  @IsString()
+  @MaxLength(50)
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(2048)
+  content: string;
+
+  @IsEnum(ArticleStatus)
+  status: ArticleStatus;
+}
