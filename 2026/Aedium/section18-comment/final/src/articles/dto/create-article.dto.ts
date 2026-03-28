b7063622abb1df80cf93061549cdde3d9116c2d0
alex <907785012@qq.com>
2026-03-28 14:55:27 +0800

    add starter of Aedium-18

---
+++ 2026/Aedium/section18-comment/final/src/articles/dto/create-article.dto.ts
@@ -0,0 +1,18 @@
+import { IsEnum, IsInt, IsNotEmpty, IsPositive, IsString, MaxLength } from 'class-validator';
+
+import { ArticleStatus } from '../entities/article.entity';
+
+export class CreateArticleDto {
+  @IsString()
+  @MaxLength(50)
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(2048)
+  content: string;
+
+  @IsEnum(ArticleStatus)
+  status: ArticleStatus;
+}
---
+++ 2026/Aedium/section18-comment/starter/src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  refreshToken?: string | null;
+}
